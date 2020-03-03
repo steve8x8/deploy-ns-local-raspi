302be423cef5f7ae8a1d923380afbb5143c4f839
Steve Sixty-Four <steve8x8@googlemail.com>
2020-03-03 18:29:25 +0100

    First shot at ARM64 (Ubuntu Bionic) install for RasPi3+

--- Readme.md
+++ Readme.md
@@ -1,38 +1,45 @@
 __Brief:__
 
-Use this script to setup a complete local running Nightscout instance. This script can either install:
-- a local MongoDB instance or
+Use this script to setup a complete local running Nightscout instance on a RasPi 3B or better, running Ubuntu Bionic.
+This script can either install:
+- a local MongoDB instance (recommended!) or
 - work without MongoDB and use static OpenAPS report files (recommended for tiny rigs) 
 
 __Tested with:__
 
-- Raspberry Pi Zero (rpi0): Working with Raspian Jessie Lite (Release date: 2016-09-23)
-- Raspberry Pi 1 Model B (rpi1): Working with Raspian Jessie Lite with PIXEL (Release date: 2016-09-23)
-- Raspberry Pi 3 (rpi3): Everything works nicely with or without PIXEL (Release date: 2016-09-23 and 2016-11-25)
+- Raspberry Pi 3B+ (rpi3b+): FIXME Everything works nicely with or without PIXEL (Release date: 2016-09-23 and 2016-11-25)
 
-__Prerequisites__
+__Prerequisites:__
 
-0. Install Raspberry Pi SD kart with Rasbian. Download Raspbian at https://www.raspberrypi.org/downloads/raspbian/
-	You can you choose to use:
-	- Rasbian Jessie with PIXEL: This has a graphical user interface, called PIXEL desktop
-	- Raspbian Jessie Lite: A minimal image based on Debian Jessie. No desktop included.
+ -1. A RasPi 4(B) must be updated with the latest (Raspbian-provided) firmware, see
+	https://jamesachambers.com/raspberry-pi-4-bootloader-firmware-updating-recovery-guide/
 
-1. Make sure your Raspberry kernel is up to date 
-   `$ sudo apt-get install rpi-update && sudo rpi-update `
-   and reboot.
+ 0. Install Raspberry Pi SD card with Debian Ubuntu "Bionic", ready for RasPi 4B from
+	https://jamesachambers.com/raspberry-pi-4-ubuntu-server-desktop-18-04-3-image-unofficial/
 
-2. Configure your Rasberry Pi
+   At the time of this writing, Version 18.04.4 was provided.
+   This comes as a server-only image; addition of desktop is possible, see James's instructions.
+
+   Username: ubuntu Password: ubuntu
+
+ 1. Configure your Raspberry Pi
    `$ sudo raspi-config`
-```   
-1. Expand Filesystem   ==> Make use of the whole SD-CARD
-2. Change User Password     	
-3. Bootoptions ==> Choose what you want
-4. Wait for Network at Boot ==> Set to No
-5.  Internationalisation Options => Change Locale, Timezone, Keyboard Layout, Wi-Fi country to your needs
-7. Advanced Options
-A2. Hostname ==> Set your hostname. This will be used for the URL of your Nightscout 
-A4. SSH ==> Enable SSH for remote access
-```
+    ```
+    1. Expand Filesystem   ==> Make use of the whole SD card
+    2. Change User Password
+    3. Bootoptions ==> Choose what you want
+    4. Wait for Network at Boot ==> Set to No
+    5. Internationalisation Options => Change Locale, Timezone, Keyboard Layout, Wi-Fi country to your needs
+    7. Advanced Options
+	A2. Hostname ==> Set your hostname. This will be used for the URL of your Nightscout
+	A4. SSH ==> Enable SSH for remote access
+    ```
+   then reboot
+
+2. Make sure your system is up to date:
+   `$ sudo apt-get update && sudo apt-get dist-upgrade -y`
+
+
 3. Tweak your Raspberry Pi.
 See for example: https://openaps.readthedocs.io/en/latest/docs/walkthrough/phase-0/rpi.html for information on setting up your Raspberry Pi:
 - Configure WiFi Settings
@@ -43,35 +50,37 @@ See for example: https://openaps.readthedocs.io/en/latest/docs/walkthrough/phase
 
 __Usage:__
 
- 1. open console on your raspi eg `ssh pi@192.168.10.4` default-password `raspberry` and run ns-local-install script for an interactive install:
+ 1. open console on your raspi eg `ssh ubuntu@192.168.10.4` (you still remember the password you set before?) and run ns-local-install script for an interactive install:
     ```
-    curl -s https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/ns-local-install.sh | bash -
-	```
-	
-	relax and drink some :coffee: - script runtime *over 1.5 hour* on clean and fresh raspi 1 or 2, and about 30 minutes on a raspi 3.
+    curl -s https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/ns-local-install.sh | bash -
+    ```
+	answer a few questions, then
+	relax and drink some :coffee: - script runtime *about 30 minutes on a raspi 3*.
 	
 	You can also use a non-interactive install:
-	```
-	mkdir src 
-    cd src
-    git clone https://github.com/SandraK82/deploy-ns-local-raspi.git
-	cd deploy-ns-local-raspi
-	```
+    ```
+    mkdir -p ns
+    cd ns
+    git clone https://github.com/steve8x8/deploy-ns-local-raspi.git
+    cd deploy-ns-local-raspi
+    git checkout arm64-ubuntu-bionic
+    ```
 	You can then use 
-	```
-	bash ns-local-install.sh [--mongo=[yes|no]] [--units=[mmol|mg]] [--storage=[openaps|mongodb]] [--oref0=[yes|no]] [--units=[mmol|mg]]
-	```
+    ```
+    bash ns-local-install.sh [--mongo=[yes|no]] [--units=[mmol|mg]] [--storage=[openaps|mongodb]] [--oref0=[yes|no]] [--units=[mmol|mg]]
+    ```
 	For example: 
-	```
-	bash ns-local-install.sh --mongo=yes --units=mmol --storage=mongo --oref0=yes
-	```
-	
+    ```
+    bash ns-local-install.sh --mongo=yes --units=mmol --storage=mongo --oref0=yes
+    ```
+
  2. after running the script you will have a running nightscout local installation. Now open editor with your config for nightscout.
-    `nano /home/pi/cgm-remote-monitor/start-nightscout.sh`
-     You need to configure at least the lines at the top of the file:
-    `CUSTOM_TITLE=mysitename_without_spaces`
-    `API_SECRET=my_12_characters_or_more_password``
-	
+    `nano /home/ubuntu/ns/start-nightscout.sh`
+    You need to configure at least the lines at the top of the file:
+    ```
+    CUSTOM_TITLE=mysitename_without_spaces
+    API_SECRET=my_12_characters_or_more_password
+    ```
     Put your personal password (at least 12 characters long) and the name of your site (just for display) there!
  
  3. once finished, restart nightscout with: `sudo /etc/init.d/nightscout stop && sudo /etc/init.d/nightscout start`
@@ -80,11 +89,17 @@ __Usage:__
 
 __Troubleshooting:__
 
- * nodejs manual start: `pi@raspberrypi:~/cgm-remote-monitor $ start-nightscout.sh` (must be in cgm-remote-monitor directory)
- * nodejs / nightscout log: check `cat /var/log/openaps/nightscout.log` 
- * mongodb check `cat /var/log/mongodb/mongodb.log` should contain: `[initandlisten] waiting for connections on port 27017`
+ * nodejs manual start: `ubuntu@raspberrypi:~/ns/cgm-remote-monitor $ ../start-nightscout.sh` (must be in cgm-remote-monitor directory)
+ * nodejs / nightscout log: check `cat /var/log/nightscout.log` 
+ * mongodb: check `cat /var/log/mongodb/mongodb.log` should contain: `[initandlisten] waiting for connections on port 27017`
 
 __Changelog:__
+
+2020-03-03:
+
+- adopted by steve8x8, for use with RasPi 3B and up, running 64-bit Ubuntu Bionic
+- use Node from APT, Mongo from mongodb.org
+- ToDo: check whether n is required at all; use/convert my.env
 
 2016-11-13:
 
@@ -92,7 +107,7 @@ __Changelog:__
 
 2016-10-14: 
 
-- change to nightscout 0.9.0 stable ()Grilled Cheese)
+- change to nightscout 0.9.0 stable (Grilled Cheese)
 - add start_nightscout.sh instead of my.env
 
 2016-09:
@@ -102,14 +117,14 @@ The patches for mongo2.x compatibility are now merged back into the official dev
 
 __With help from:__
 
+- https://github.com/SandraK82/deploy-ns-local-raspi
 - https://c-ville.gitbooks.io/test/content/
 - http://yannickloriot.com/2016/04/install-mongodb-and-node-js-on-a-raspberry-pi/
 - https://www.einplatinencomputer.com/raspberry-pi-node-js-installieren/
 - contributions from PieterGit
 
-__Whishlist/To Do:__
-- seperate username/password for Mongo
+__Wishlist/To Do:__
+- separate username/password for Mongo
 - Nginx to use for https / letsencrypt certificate
 - Script to create wifi hotspot on the raspberry pi
-- Always install latest Node (now 6.8.0 instead of 6.7.0 what is being installed)
 - ...
--- ns-local-install.sh
+++ ns-local-install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-## from https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/ns-local-install.sh
+## from https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/ns-local-install.sh
 
 ## TODO: set /etc/domainname
+
+## Note: whole script to be run with user privileges
+
+# check whether this is the right arch
+CPU_MODEL=$(uname -m)
+if [ "$CPU_MODEL" = "aarch64" ]
+then
+  echo "ARM64 architecture (\"aarch64\") confirmed"
+else
+  echo "Wrong platform. Found \"${CPU_MODEL}\", need \"aarch64\". Exit."
+  exit 1
+fi
 
 # make me current
 sudo apt-get update && sudo apt-get upgrade -y
@@ -12,25 +24,25 @@ for i in "$@"
 do
 case $i in
     --mongo=*)
-    INSTALL_MONGO="${i#*=}"
-    shift # past argument=value
-    ;;
+	INSTALL_MONGO="${i#*=}"
+	shift # past argument=value
+	;;
     --units=*)
-    UNITS="${i#*=}"
-    shift # past argument=value
-    ;;
+	UNITS="${i#*=}"
+	shift # past argument=value
+	;;
     --storage=*)
-    STORAGE="${i#*=}"
-    shift # past argument=value
-    ;;
+	STORAGE="${i#*=}"
+	shift # past argument=value
+	;;
     --oref0=*)
-    INSTALL_OREF0="${i#*=}"
-    shift # past argument=value
-    ;;
+	INSTALL_OREF0="${i#*=}"
+	shift # past argument=value
+	;;
     *)
-    # unknown option
-    echo "Option ${i#*=} unknown"
-    ;;
+	# unknown option
+	echo "Option ${i#*=} unknown"
+	;;
 esac
 done
 
@@ -55,14 +67,12 @@ if ! [[ ${STORAGE,,} =~ "openaps" || ${STORAGE,,} =~ "mongo" ]]; then
     STORAGE="" # to force a Usage prompt
 fi
 
-
 if ! [[ ${INSTALL_OREF0,,} =~ "yes" || ${INSTALL_OREF0,,} =~ "no"  ]]; then
     echo ""
     echo "Unsupported value for --oref0. Choose either 'yes' or 'no'. "
     echo
     INSTALL_OREF0="" # to force a Usage prompt
 fi
-
 
 if [[ -z "$INSTALL_MONGO" || -z "$UNITS" || -z "$STORAGE" || -z "$INSTALL_OREF0" ]]; then
     echo "Usage: ns-local-install.sh [--mongo=[yes|no]] [--units=[mmol|mg]] [--storage=[openaps|mongo]] [--oref0=[yes|no]]"
@@ -71,75 +81,56 @@ if [[ -z "$INSTALL_MONGO" || -z "$UNITS" || -z "$STORAGE" || -z "$INSTALL_OREF0"
         exit
     fi
 
-	while true; do
-	    read -p "Do you want to install MongoDB? [Y]/n" -r
-		case $REPLY in
-			"") INSTALL_MONGO="yes" ; break;;
-			[Yy]* ) INSTALL_MONGO="yes" ; break;;
-			[Nn]* ) INSTALL_MONGO="no" ; break;;
-			* ) echo "Please answer yes or no";;
-		esac
-	done
-	
-	while true; do
-    read -p "Do you want to use mmol or mg [mmol]/mg]? " unit
-    case $unit in
-		"") UNITS="mmol" ; break;;
-        mmol) UNITS="mmol"; break;;
-        mg) UNITS="mg"; break;;
-        * ) echo "Please answer mmol or mg.";;
+    while true; do
+	read -p "Do you want to install MongoDB? [Y]/n" -r
+	case $REPLY in
+		"") INSTALL_MONGO="yes" ; break;;
+		[Yy]* ) INSTALL_MONGO="yes" ; break;;
+		[Nn]* ) INSTALL_MONGO="no" ; break;;
+		* ) echo "Please answer yes or no";;
 	esac
-	done
+    done
 
-	echo "Nightscout has two options for storage:"
-	echo "openaps: Nightscout will use the OpenAPS files"
-	echo "mongodb: Nightscout will use a MongoDB"
-	while true; do
-    read -p "What storage do you want to use? Choose [mongodb] / openaps " storage
-    case $storage in
-		"") STORAGE="mongo" ; break;;
-        mongodb) STORAGE="mongo"; break;;
-        openaps) STORAGE="openaps"; break;;
-        * ) echo "Please answer mongo or openaps. ";;
+    while true; do
+	read -p "Do you want to use mmol or mg [mmol]/mg]? " unit
+	case $unit in
+		"") UNITS="mmol" ; break;;
+		mmol) UNITS="mmol"; break;;
+		mg) UNITS="mg"; break;;
+		* ) echo "Please answer mmol or mg.";;
 	esac
-	done
+    done
+
+    echo "Nightscout has two options for storage:"
+    echo "openaps: Nightscout will use the OpenAPS files"
+    echo "mongodb: Nightscout will use a MongoDB"
+    while true; do
+	read -p "What storage do you want to use? Choose [mongodb] / openaps " storage
+	case $storage in
+		"") STORAGE="mongo" ; break;;
+		mongodb) STORAGE="mongo"; break;;
+		openaps) STORAGE="openaps"; break;;
+		* ) echo "Please answer mongo or openaps. ";;
+	esac
+    done
 
     read -p " " -r
 	=$REPLY
 	
-	while true; do
-		read -p "Do you wish to install OpenAPS basic oref0? [Y]/n" yn
-		case $yn in
-			[Yy]* ) break;;
-			[Nn]* ) break;;
-		esac
-	done
-	INSTALL_OREF0=$yn
-	
+    while true; do
+	read -p "Do you wish to install OpenAPS basic oref0? [Y]/n" yn
+	case $yn in
+		[Yy]* ) break;;
+		[Nn]* ) break;;
+	esac
+    done
+    INSTALL_OREF0=$yn
+
 fi
 
 # get the right node
-CPU_MODEL=$( awk '/model name/ {print $4}' < /proc/cpuinfo )
-if [ "$CPU_MODEL" = "ARMv6-compatible" ]
-then
-  echo "ARMv6 detected"
-  # install node (on ARMv6 eg. Raspberry Model A/B/B+/A+/Zero)
-  wget https://nodejs.org/dist/v6.7.0/node-v6.7.0-linux-armv6l.tar.xz
-  tar -xvf node-v6.7.0-linux-armv6l.tar.xz
-  cd node-v6.7.0-linux-armv6l
-  sudo cp -R * /usr/local/
-  # check version should be v6.7.0
-  node -v
-  cd ..
-  # clean up
-  rm node-v6.7.0-linux-armv6l.tar.xz
-  rm -r node-v6.7.0-linux-armv6l
-else
-  echo "Assuming ARMv8 (Raspi 3))"
-  # install node (on ARMv8 eg Raspberry 3 Model B)
-  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-fi
+# Ubuntu Bionic: 8.10
+sudo apt-get install -y nodejs
 
 # install dependencies 
 # get git, mongodb 2.x from apt for now,and npm
@@ -147,49 +138,49 @@ fi
 EXTRAS="etckeeper tcsh lsof"
 sudo apt-get install --assume-yes git npm $EXTRAS
 
-if [[ ${INSTALL_MONGO,,} =~ "yes" || ${INSTALL_MONGO,,} =~ "y"  ]]; then
-	sudo apt-get install --assume-yes git mongodb-server
-	# enable mongo
-	sudo systemctl enable mongodb.service
-	# check mongo status
-	sudo systemctl status mongodb.service
-	# get log of mongo
-	#cat /var/log/mongodb/mongodb.log -> should contain: [initandlisten] waiting for connections on port 27017
+if [[ ${INSTALL_MONGO,,} =~ "yes" || ${INSTALL_MONGO,,} =~ "y"  ]]
+then
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+    sudo apt-get update
+    sudo apt-get install mongodb-org -y
+    # enable mongo
+    sudo systemctl enable mongodb.service
+    # check mongo status
+    sudo systemctl status mongodb.service
+    # get log of mongo
+    tail -n10  /var/log/mongodb/mongodb.log
+    grep 27017 /var/log/mongodb/mongodb.log
+    echo "log should contain: [initandlisten] waiting for connections on port 27017"
 fi
 
 sudo npm cache clean -f
-sudo npm install npm -g
-sudo npm install n -g
-
-# select matching node
-sudo n 4.6
+#sudo npm install npm -g
+#sudo npm install n -g
+#sudo n lts
 
 # go home
-cd
+mkdir -p ~/ns
+cd ~/ns
 
 # get start script
 case $UNITS in
-   mmol) curl -o start_nightscout.sh https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/start_nightscout.sh; break;;
-   mg) curl -o start_nightscout.sh https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/start_nightscout-mg.sh; break;;
+   mmol) curl -o start_nightscout.sh https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/start_nightscout.sh;;
+   mg)   curl -o start_nightscout.sh https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/start_nightscout-mg.sh;;
 esac
-
 chmod +rx start_nightscout.sh
 
 git clone https://github.com/nightscout/cgm-remote-monitor.git
-
 # switching to cgm-remote-monitor directory
-cd cgm-remote-monitor/
-
+pushd cgm-remote-monitor/
 # switch to dev (latest development version)
 git checkout dev
-
 # setup ns
 ./setup.sh
-
+popd
 
 # make autoboot
-cd
-curl -o nightscout https://raw.githubusercontent.com/SandraK82/deploy-ns-local-raspi/master/nightscout
+curl -o nightscout https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/nightscout
 sudo mv nightscout /etc/init.d/nightscout
 sudo chmod +x /etc/init.d/nightscout
 sudo /etc/init.d/nightscout start
@@ -197,8 +188,8 @@ sudo /etc/init.d/nightscout status
 sudo insserv -d nightscout
 
 echo "deploy nightscout on raspi done :)"
-echo "Dont forget to edit: /home/pi/cgm-remote-monitor/start_nightscout.sh"
-echo "Nightscout logging can be found at: /var/log/openaps/nightscout.log"
+echo "Don't forget to edit: /home/ubuntu/start_nightscout.sh"
+echo "Nightscout logging can be found at: /var/log/nightscout.log"
 
 case $OREF0 in
         [Yy]* ) break;;
@@ -208,8 +199,7 @@ esac
 # Setup basis oref0 stuff
 # https://openaps.readthedocs.io/en/master/docs/walkthrough/phase-2/oref0-setup.html
 curl -s https://raw.githubusercontent.com/openaps/docs/master/scripts/quick-packages.sh | bash -
-
-mkdir -p ~/src; cd ~/src && git clone -b dev git://github.com/openaps/oref0.git || (cd oref0 && git checkout dev && git pull)
+git clone -b dev git://github.com/openaps/oref0.git || (cd oref0 && git checkout dev && git pull)
 
 echo "Please continue with step 2 of https://openaps.readthedocs.io/en/master/docs/walkthrough/phase-2/oref0-setup.html"
-echo "cd && ~/src/oref0/bin/oref0-setup.sh"
+echo "cd && ~/ns/oref0/bin/oref0-setup.sh"
