3c31ecdf329ec2e9502aff12468e1aeb39c1f3ea
Steve Sixty-Four <steve8x8@googlemail.com>
2020-03-04 12:00:55 +0100

    Added consistency checks, on-the-fly substitutions, use local copy of files.

--- ns-local-install.sh
+++ ns-local-install.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-## from https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/ns-local-install.sh
+NSHOME=~/ns
 
-## TODO: set /etc/domainname
+## TODO: set /etc/domainname (why?)
+## TODO: use my.key from zehn.be export
 
 ## Note: whole script to be run with user privileges
 
+## Some consistency checks.
 # check whether this is the right arch
-CPU_MODEL=$(uname -m)
-if [ "$CPU_MODEL" = "aarch64" ]
+echo Checking CPU architecture...
+CPU_MODEL=`uname -m`
+if [ "${CPU_MODEL}" = "aarch64" ]
 then
-  echo "ARM64 architecture (\"aarch64\") confirmed"
+    echo "ARM64 architecture (\"aarch64\") confirmed."
 else
-  echo "Wrong platform. Found \"${CPU_MODEL}\", need \"aarch64\". Exit."
-  exit 1
+    echo "Wrong platform. Found \"${CPU_MODEL}\", need \"aarch64\". Exit."
+    exit 1
+fi
+
+echo Checking OS version...
+OS_VERSION=`. /etc/os-release 2>/dev/null; echo ${VERSION_CODENAME}`
+if [ "${OS_VERSION}" = "bionic" ]
+then
+    echo "Ubuntu Bionic (\"bionic\") confirmed."
+else
+    echo "Wrong OS. Found \"${OS_VERSION}\", need \"bionic\". Exit."
+    exit 1
+fi
+
+echo Checking memory and swap...
+TOTAL_MB=`free 2>/dev/null | awk '{s+=$2+0}END{printf("%.0f\n",s/1024)}'`
+if [ ${TOTAL_MB} -ge 2048 ]
+then
+    echo "Memory looks sufficient (${TOTAL_MB} MiB found)."
+else
+    echo "Memory not sufficient (${TOTAL_MB} MiB found). 2048 MiB or more recommended. Add swap space."
+    exit 1
 fi
 
 # make me current
@@ -114,29 +137,24 @@ if [[ -z "$INSTALL_MONGO" || -z "$UNITS" || -z "$STORAGE" || -z "$INSTALL_OREF0"
 	esac
     done
 
-    read -p " " -r
-	=$REPLY
-	
     while true; do
-	read -p "Do you wish to install OpenAPS basic oref0? [Y]/n" yn
-	case $yn in
-		[Yy]* ) break;;
-		[Nn]* ) break;;
+	read -p "Do you wish to install OpenAPS basic oref0? [N]/y" -r
+	case $REPLY in
+		"") INSTALL_OREF0="no" ; break;;
+		[Yy]* ) INSTALL_OREF0="yes" ; break;;
+		[Nn]* ) INSTALL_OREF0="no" ; break;;
+		* ) echo "Please answer yes or no";;
 	esac
     done
-    INSTALL_OREF0=$yn
 
 fi
 
-# get the right node
-# Ubuntu Bionic: 8.10
-sudo apt-get install -y nodejs
-
 # install dependencies 
-# get git, mongodb 2.x from apt for now,and npm
+# get git, mongodb community edition, and npm
 # optional extra packages to easily debug stuff or to do better maintenance
 EXTRAS="etckeeper tcsh lsof"
-sudo apt-get install --assume-yes git npm $EXTRAS
+EXTRAS=""
+sudo apt-get install --assume-yes git npm ${EXTRAS}
 
 if [[ ${INSTALL_MONGO,,} =~ "yes" || ${INSTALL_MONGO,,} =~ "y"  ]]
 then
@@ -145,61 +163,78 @@ then
     sudo apt-get update
     sudo apt-get install mongodb-org -y
     # enable mongo
-    sudo systemctl enable mongodb.service
+    sudo systemctl enable mongod.service
     # check mongo status
-    sudo systemctl status mongodb.service
+    sudo systemctl status mongod.service
+    # restart just in case
+    sudo systemctl restart mongod.service
     # get log of mongo
-    tail -n10  /var/log/mongodb/mongodb.log
-    grep 27017 /var/log/mongodb/mongodb.log
-    echo "log should contain: [initandlisten] waiting for connections on port 27017"
+    sudo tail -n10  /var/log/mongodb/mongod.log
+    sudo grep 27017 /var/log/mongodb/mongod.log
+    echo "log should contain a fresh record: [listener] waiting for connections on port 27017"
 fi
 
-sudo npm cache clean -f
-#sudo npm install npm -g
-#sudo npm install n -g
-#sudo n lts
-
-# go home
-mkdir -p ~/ns
-cd ~/ns
+# create NS home if not yet there
+mkdir -p ${NSHOME}
 
 # get start script
-case $UNITS in
-   mmol) curl -o start_nightscout.sh https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/start_nightscout.sh;;
-   mg)   curl -o start_nightscout.sh https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/start_nightscout-mg.sh;;
-esac
-chmod +rx start_nightscout.sh
+# FIXME: use my.key instead
+#curl -o ${NSHOME}/start_nightscout.sh https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/start_nightscout.sh
+cp -p start_nightscout.sh ${NSHOME}/
+[ "$UNITS" = "mg" ] && sed -i -e 's~=mmol~=mg/dl~g' ${NSHOME}/start_nightscout.sh
+chmod +rx ${NSHOME}/start_nightscout.sh
+# init script
+#curl -o ${NSHOME}/nightscout.init https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/nightscout.init
+cp -p nightscout.init ${NSHOME}/
+sed -i -e "s~XXXNSHOMEXXX~${NSHOME}~g" ${NSHOME}/nightscout.init
 
-git clone https://github.com/nightscout/cgm-remote-monitor.git
+cd ${NSHOME}
+# get latest copy
+[ -d cgm-remote-monitor ] || git clone https://github.com/nightscout/cgm-remote-monitor.git
 # switching to cgm-remote-monitor directory
 pushd cgm-remote-monitor/
 # switch to dev (latest development version)
 git checkout dev
-# setup ns
+git pull
+
+# we do not neet to get the right node
+# Ubuntu Bionic comes with 8.10
+# NS would like to install the latest 8.x from NodeSource
+#sudo apt-get install -y nodejs
+#sudo npm cache clean -f
+#sudo npm install npm -g
+#sudo npm install n -g
+#sudo n lts
+
+# setup ns: this installs nodejs from nodesource!
 ./setup.sh
 popd
 
-# make autoboot
-curl -o nightscout https://raw.githubusercontent.com/steve8x8/deploy-ns-local-raspi/arm64-ubuntu-bionic/nightscout
-sudo mv nightscout /etc/init.d/nightscout
+# start at boot
+sudo cp -p ${NSHOME}/nightscout.init /etc/init.d/nightscout
 sudo chmod +x /etc/init.d/nightscout
 sudo /etc/init.d/nightscout start
 sudo /etc/init.d/nightscout status
 sudo insserv -d nightscout
 
 echo "deploy nightscout on raspi done :)"
-echo "Don't forget to edit: /home/ubuntu/start_nightscout.sh"
+echo "Don't forget to edit: ${NSHOME}/start_nightscout.sh"
 echo "Nightscout logging can be found at: /var/log/nightscout.log"
 
-case $OREF0 in
+case $INSTALL_OREF0 in
         [Yy]* ) break;;
         [Nn]* ) exit;;
 esac
 
-# Setup basis oref0 stuff
+# Setup basic oref0 stuff
 # https://openaps.readthedocs.io/en/master/docs/walkthrough/phase-2/oref0-setup.html
 curl -s https://raw.githubusercontent.com/openaps/docs/master/scripts/quick-packages.sh | bash -
-git clone -b dev git://github.com/openaps/oref0.git || (cd oref0 && git checkout dev && git pull)
+#git clone -b dev git://github.com/openaps/oref0.git || (cd oref0 && git checkout dev && git pull)
+[ -d oref0 ] || git clone -b dev https://github.com/openaps/oref0.git
+pushd oref0
+git checkout dev
+git pull
+popd
 
 echo "Please continue with step 2 of https://openaps.readthedocs.io/en/master/docs/walkthrough/phase-2/oref0-setup.html"
-echo "cd && ~/ns/oref0/bin/oref0-setup.sh"
+echo "cd && ${NSHOME}/oref0/bin/oref0-setup.sh"
